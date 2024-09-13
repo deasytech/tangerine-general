b7710c6f5f71e49b61e0ee199b9fa19b5e0816fe
Desmond Awere <desmondawere@gmail.com>
2024-09-13 02:09:01 +0100

    navbar glitch fixed

--- components/NavbarMenu.tsx
+++ components/NavbarMenu.tsx
@@ -26,33 +26,32 @@ export function NavbarMenu() {
                 <NavigationMenuTrigger>{link.label}</NavigationMenuTrigger>
                 <NavigationMenuContent>
                   <div className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                    {link.key === "about-us" && (
+                    {link.key === "corporate-plans" && (
                       <NavigationMenuLink asChild>
                         <a
                           className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                           href="/"
                         >
                           <div className="mb-2 mt-4 text-lg font-medium">
-                            About Tangerine Financial
+                            About Tangerine General
                           </div>
                           <p className="text-sm leading-tight text-muted-foreground">
-                            Tangerine Financial is an investment holding company with registration number 13341620.
+                            At Tangerine General, we're more than just an insurance company
                           </p>
                         </a>
                       </NavigationMenuLink>
                     )}
-                    {link.key === "our-subsidiaries" && (
+                    {link.key === "retail-plans" && (
                       <NavigationMenuLink asChild>
                         <a
                           className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                           href="/"
                         >
                           <div className="mb-2 mt-4 text-lg font-medium">
-                            Our Subsidiaries
+                            Retail Plans
                           </div>
                           <p className="text-sm leading-tight text-muted-foreground">
-                            We invest in businesses that merge deep consumer insights and cutting-edge technology to provide a range of carefully
-                            tailored products that improve life.
+                            Our retail plans offer tailored solutions for diverse needs
                           </p>
                         </a>
                       </NavigationMenuLink>
--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 import { ChevronDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import MobileNavbar from "@/components/general/MobileNavbar";
+import { NavbarMenu } from "../NavbarMenu";
 
 const Navbar = () => {
   const pathname = usePathname();
@@ -46,45 +47,7 @@ const Navbar = () => {
           />
         </Link>
 
-        <ul className="hidden items-center h-full gap-4 lg:flex">
-          {NAV_LINKS.map((link) => (
-            <li key={link.key} className="relative">
-              {link.subMenu ? (
-                <button
-                  onClick={() => handleMenuClick(link.key)}
-                  className="regular-12 text-blue-base flexCenter cursor-pointer transition-all hover:bg-orange-50 p-1 px-4 hover:rounded-full"
-                >
-                  {link.label}
-                  <ChevronDown className="ml-2" size={16} />
-                </button>
-              ) : (
-                <Link
-                  href={link.href}
-                  key={link.key}
-                  onClick={handleSubMenuClick}
-                  className={`regular-12 text-blue-base flexCenter cursor-pointer transition-all hover:bg-orange-50 p-1 px-4 hover:rounded-full ${pathname === link.href ? 'bg-orange-base rounded-full text-white hover:text-blue-base' : ''}`}
-                >
-                  {link.label}
-                </Link>
-              )}
-              {link.subMenu && activeMenu === link.key && (
-                <div className="absolute left-0 top-full mt-2 w-[333px] bg-white shadow-lg rounded-xl p-4">
-                  <ul className="flex gap-8">
-                    <div className="w-full flex flex-col gap-2">
-                      {link.subMenu.map((subLink) => (
-                        <li key={subLink.key} className="mb-2 p-2 last:mb-0 hover:bg-orange-50">
-                          <Link href={subLink.href} className="regular-12 text-generic-700" onClick={handleSubMenuClick}>
-                            {subLink.label}
-                          </Link>
-                        </li>
-                      ))}
-                    </div>
-                  </ul>
-                </div>
-              )}
-            </li>
-          ))}
-        </ul>
+        <NavbarMenu />
 
         <div className="lg:flexCenter hidden gap-4">
           <Link href="/get-quote">
