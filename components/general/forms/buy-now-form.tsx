5e3616ffb86b519f585952b5a9d8a0517207df05
Desmond Awere <desmondawere@gmail.com>
2024-10-14 19:40:34 +0100

    quote alert updated

--- components/general/forms/buy-now-form.tsx
+++ components/general/forms/buy-now-form.tsx
@@ -90,18 +90,8 @@ export function GetQuoteForm() {
 
       toast({
         title: "Data submitted successfully",
-        description: (
-          <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-            <div className="text-white">
-              {Object.entries(data).map(([ key, value ]) => (
-                <div key={key} className="flex justify-between mb-2">
-                  <span className="font-semibold">{key}:</span>
-                  <span>{value}</span>
-                </div>
-              ))}
-            </div>
-          </pre>
-        ),
+        description: "We will contact you with more information in 24 hours.",
+        variant: "default",
       });
 
       form.reset(defaultValues);
